50c2c4b5562259ba91e913aac9ee9373e76f9f20
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-01 16:46:52 +0200

    editing

--- src/stores/useCounterStore.js
+++ src/stores/useCounterStore.js
@@ -1,23 +1,29 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-export const useCounterStore = create(persist(set) => ({
-  count: 0,
-  increment: () => {
-    set((state) => ({ count: state.count + 1 }));
-  },
-  decrement: () => {
-    set((state) => ({
-      count: state.count - 1,
-    }));
-  },
-  resetCount: () => {
-    set(() => ({
-      count: 0,
-    }));
-  },
-},
-{
-    name: "set-counter",
-    getStorage: () => localStorage
-}));
+export const useCounterStore = create(
+  persist(
+    (set) => (
+      {
+        count: 0,
+        increment: () => {
+          set((state) => ({ count: state.count + 1 }));
+        },
+        decrement: () => {
+          set((state) => ({
+            count: state.count - 1,
+          }));
+        },
+        resetCount: () => {
+          set(() => ({
+            count: 0,
+          }));
+        },
+      },
+      {
+        name: "set-counter",
+        getStorage: () => localStorage,
+      }
+    )
+  )
+);
