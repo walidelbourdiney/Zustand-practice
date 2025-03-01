9f1a247166ac57ca2b78645f6ec963b8e2b0287c
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-01 15:17:59 +0200

    make counter using zustand library

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { useCounterStore } from "./stores/useCounterStore";
+
+function App() {
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </>
+  );
+}
+
+export default App;
