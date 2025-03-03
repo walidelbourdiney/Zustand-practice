a55507c62b286b388de3d7f336f33e1b63563f11
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-03 11:48:02 +0200

    keep practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
-import { useCounterStore } from "./stores/useCounterStore";
+import {useCounterStore} from './stores/useCounterStore'
 
 function App() {
-  const count = useCounterStore((state) => state.count);
-  const increment = useCounterStore((state) => state.increment);
-  const decrement = useCounterStore((state) => state.decrement);
-  const resetCount = useCounterStore((state) => state.resetCount);
-  console.log(count)
+ const {count, increment, decrement, resetCount} = useCounterStore();
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={increment} className="bg-blue-500">+</button>
+      <button onClick={increment} className="bg-red-500">+</button>
       <button onClick={decrement}>-</button>
       <button onClick={resetCount}>Reset</button>
     </>
