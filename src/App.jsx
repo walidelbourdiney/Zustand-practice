e6f43b0d2e3e228a1a9d0bf474a52a1a3523e63a
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-04 05:09:44 +0200

    practice zustand

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 function App() {
- const {count, increment, decrement, resetCount} = useCounterStore();
   return (
     <>
       <RecipeList />
--- src/stores/useRecipeStore.js
+++ src/stores/useRecipeStore.js
@@ -3,6 +3,12 @@ import { create } from "zustand";
 export const useRecipeStore = create((set) => ({
     recipes: [],
     addRecipe: (newRecipe) => set((state) => ({recipes: [...state.recipes, newRecipe]})),
-    setRecipe: (recipes) => set({recipes})
+    setRecipe: (recipes) => set({recipes}),
+    deleteRecipe: (id) => set(state => ({recipes: state.recipes.filter(recipe.id !== id)})),
+    updateRecipe: (updatedRecipe) => set((state) => ({
+        recipes: state.recipes.map(recipe => 
+            recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+        )
+    })),
 
 }))
