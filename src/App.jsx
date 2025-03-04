96824899d68e56966e17c9bfca796ead2dca4e1e
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-04 13:01:39 +0200

    practice formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import Form from "./components/Form";
 function App() {
   return (
     <>
-      <RecipeList />
-      <AddRecipeForm />
+     <Form />
     </>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Formik,  Field, useFormik} from 'formik';
+import * Yup from 'yup';
+
+const Form = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            channel: ""
+        },
+        onSubmit: values => {
+            console.log(values)
+        },
+        validate: values =>{
+            let errors = {};
+
+            if(!values.name){
+                errors.name = "Required"
+            }
+            if(!values.email){
+                errors.email = "Required"
+            }
+            return errors;
+      
+    }})
+  return (
+    <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
+        {formik.errors.name? <div>{formik.errors.name}</div> : null}
+        <label htmlFor="email">Email</label>
+        <input type="text" id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
+        {formik.errors.email? <div>{formik.errors.email}</div> : null}
+
+        <label htmlFor="channel">Channel</label> 
+        <input type="text" id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
+
+        <button type='submit'>Submit</button>
+    </form>
+  )
+}
+
+export default Form
